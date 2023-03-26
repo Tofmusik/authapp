800ef41a8226c12a77b33348ee42b9a720c37f1d
Tofmusik <musik94700@gmail.com>
2023-03-26 20:27:09 +0200

    Modif Layout et ajout icons

--- layout/layout.js
+++ layout/layout.js
@@ -1,13 +1,14 @@
-
+import Image from "next/image";
+import Logo from "../public/assets/logo.png";
 
 const Layout = ({ children }) => {
     return (
         <div className="flex h-screen bg-blue-400">
-            <div className="m-auto bg-gradient-to-t from-slate-900 via-blue-900 to-slate-900 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
-                <div>
-                    images
+            <div className="m-auto bg-gray-50 rounded-md w-3/5 h-auto grid lg:grid-cols-2">
+                <div className="h-full flex justify-center items-center bg-gradient-to-t from-slate-900 via-blue-900 to-slate-900 rounded-l-md">
+                    <Image src={Logo} width={250} height={250} alt="Logo"/>
                 </div>
-                <div className="right flex flex-col justify-evenly bg-gray-500">
+                <div className="right flex flex-col justify-evenly">
                     <div className="text-center py-10">
                         {children}
                     </div>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,13 +1,76 @@
 import Head from "next/head";
 import Layout from "../../layout/layout";
+import Link from "next/link";
+import styles from '../styles/Form.module.css';
+import Image from "next/image";
+import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from "react-icons/hi";
+import { useState } from 'react';
 
 const Register = () => {
+
+    const[show, setShow] = useState({password:false, cpassword:false} )
+
     return (
         <Layout>
             <Head>
                 <title>Register</title>
             </Head>
-            <h1>Register</h1>
+            <section className="w-3/4 mx-auto flex flex-col gap-10">
+                <div className="title">
+                    <h1 className="text-gray-800 text-4xl font-bold py-4">Register</h1>
+                    <p className="w-3/4 mx-auto text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+
+                {/*form*/}
+                <form className="flex flex-col gap-5">
+                    <div className={styles.input_group}>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            className={styles.input_text}
+                        />
+                        <span className="icon flex items-center px-4"><HiOutlineUser size={25}/></span>
+                    </div>
+                    <div className={styles.input_group}>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className={styles.input_text}
+                        />
+                        <span className="icon flex items-center px-4"><HiAtSymbol size={25}/></span>
+                    </div>
+                    <div className={styles.input_group}>
+                        <input
+                            type={`${show.password ? "text" : "password"}`}
+                            name="password"
+                            placeholder="Password"
+                            className={styles.input_text}
+                        />
+                        <span className="icon flex items-center px-4" onClick={() => setShow({...show, password: !show.password})}><HiFingerPrint size={25}/></span>
+                    </div>
+                    <div className={styles.input_group}>
+                        <input
+                            type={`${show.cpassword ? "text" : "password"}`}
+                            name="password"
+                            placeholder="Confirm Password"
+                            className={styles.input_text}
+                        />
+                        <span className="icon flex items-center px-4" onClick={() => setShow({...show, cpassword: !show.cpassword})}><HiFingerPrint size={25}/></span>
+                    </div>
+                    {/*login buttons*/}
+                    <div className={styles.login_button}>
+                        <button type="submit">
+                            Login
+                        </button>
+                    </div>
+                </form>
+                {/*bottom*/}
+                <p className="text-center text-gray-400">
+                    Have an account ? <Link href={'/login'} className='text-blue-700'>Sign In</Link>
+                </p>
+            </section>
         </Layout>
     )
 }
